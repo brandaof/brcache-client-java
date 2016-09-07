ef8e1b8b6a22fdef74a06f8a7a96dae2a825e64f
Afonso Brandao <brandaof@users.noreply.github.com>
2016-09-07 21:05:57 +0000

    #

--- src/main/java/org/brandao/brcache/client/BrCacheConnectionImp.java
+++ src/main/java/org/brandao/brcache/client/BrCacheConnectionImp.java
@@ -209,12 +209,12 @@ class BrCacheConnectionImp implements BrCacheConnection{
 
     	try{
 	    	this.sender.executePut(key, timeToLive, timeToIdle, value);
-	        //return this.receiver.processPutResult();
-    		return true;
+	        return this.receiver.processPutResult();
+    		//return true;
     	}
-		//catch(CacheException e){
-		//	throw e;
-		//}
+		catch(CacheException e){
+			throw e;
+		}
     	catch(Throwable e){
     		throw new CacheException(e);
     	}
